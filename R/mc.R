2a834be9ae5afcce5a3ebc7d601b0700659836c8
drlevy <daniel.levy@gmail.com>
2016-01-20 01:00:13 -0800

    TEST

--- R/mc.R
+++ R/mc.R
@@ -1,7 +1,7 @@
 #' Markov chain definition.
 #' @param Construct and initialize with some pijdef.
 #' @slot pijdef is either a square finite transition matrix
-#' or a function(i, j) defining a potentially infinite transition matrix.
+#' or a function(i, j) defining a (potentially) infinite transition matrix.
 #' @export
 mc <- function(pijdef = NULL) {
   mc <- list(pijdef = pijdef)
