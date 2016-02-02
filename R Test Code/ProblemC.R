c58a066ae994c8fffb71dff8d7e1c9dfa03c3768
cdhenderson <cdhenderson@bitbucket.org>
2016-02-01 23:25:51 -0800

    Refactored the simulation code and updated the latex document to have the simulation section.

--- R Test Code/ProblemC.R
+++ R Test Code/ProblemC.R
@@ -12,10 +12,17 @@ ProblemCSimulation <- function(servers, queue_size, portion_complex, arrival_rat
     {
       for(k in 0:(queue_size))
       {
-        if(i == 2 && j == 1)
-        {
-          stop = TRUE
-        }
+        give_to_i = FALSE
+        take_from_i = FALSE
+        
+        give_to_j = FALSE
+        take_from_j = FALSE
+        
+        give_to_k = FALSE
+        take_from_k = FALSE
+        
+        complex_give_to_j = FALSE
+        
         state_i = GenerateStateIndex(i,j,k,queue_size)
         pij[state_i, state_i] = 0
         qi[state_i] = GenerateProbability(i,j,k,queue_size, arrival_rate, service_rate, m_service_rate, customer_leave_rate)
@@ -40,36 +47,22 @@ ProblemCSimulation <- function(servers, queue_size, portion_complex, arrival_rat
             {
               if( k == 0 )
               {
-                state_j = GenerateStateIndex(i-1, j, 0, queue_size)
-                pij[state_i,state_j] = i*(service_rate * sum_prob)
+                take_from_i = TRUE
               }
               else
               {
-                for(z in 2:queue_size)
-                {
-                  if( (k - z) > 0 )
-                  {
-                    state_j = GenerateStateIndex(i, j+z-1, k-z, queue_size)
-                    pij[state_i,state_j] = sum_prob*i*(portion_complex^(z-1))*(1.0-portion_complex)
-                    
-                  }
-                }
-                
-                state_j = GenerateStateIndex(i-1, j+k, 0, queue_size)
-                pij[state_i,state_j] = sum_prob*i*service_rate*(portion_complex^k)
+                complex_give_to_j = TRUE
               }
             }
             
             if( j != 0 )
             {
-              state_j = GenerateStateIndex(i, j-1, k, queue_size)
-              pij[state_i,state_j] = (m_service_rate * sum_prob)
+              take_from_j = TRUE
             }
             
             if( k != 0 )
             {
-              state_j = GenerateStateIndex(i, j, k-1, queue_size)
-              pij[state_i,state_j] = ((customer_leave_rate +(i*service_rate*(1.0-portion_complex))) * sum_prob)
+              take_from_k = TRUE
             }
             
           }
@@ -79,29 +72,23 @@ ProblemCSimulation <- function(servers, queue_size, portion_complex, arrival_rat
             {
                 if( i != servers )
                 {
-                  state_j = GenerateStateIndex(i+1, j, k, queue_size)
-                  pij[state_i,state_j] = (arrival_rate * sum_prob)*(1.0-portion_complex)
-                  
-                  state_j = GenerateStateIndex(i, j+1, k, queue_size)
-                  pij[state_i,state_j] = (arrival_rate * sum_prob)*portion_complex
+                  give_to_i = TRUE
+                  give_to_j = TRUE
                 }
                 else
                 {
-                  state_j = GenerateStateIndex(i, j, k+1, queue_size)
-                  pij[state_i,state_j] = (arrival_rate * sum_prob)
+                  give_to_k = TRUE
                 }
                
                 
                 if( i != 0 )
                 {
-                  state_j = GenerateStateIndex(i-1, j, k, queue_size)
-                  pij[state_i,state_j] = sum_prob*i*service_rate
+                  take_from_i = TRUE
                 }
                 
                 if( j != 0 )
                 {
-                  state_j = GenerateStateIndex(i, j-1, k, queue_size)
-                  pij[state_i,state_j] = (m_service_rate * sum_prob)
+                  take_from_j = TRUE
                 }
             }
             else if( (k > 0) && (i == servers)) # can't have a queue with servers available
@@ -109,34 +96,71 @@ ProblemCSimulation <- function(servers, queue_size, portion_complex, arrival_rat
               #i,k are both > 0
               if( j != 0 )
               {
-                state_j = GenerateStateIndex(i, j-1, k, queue_size)
-                pij[state_i,state_j] = (m_service_rate * sum_prob)
+                take_from_j = TRUE
               }
               
               if( k != 0 )
               {
-                state_j = GenerateStateIndex(i, j, k-1, queue_size)
-                pij[state_i,state_j] = sum_prob*(customer_leave_rate + (i*service_rate*(1.0-portion_complex)))
+                take_from_k = TRUE
+                complex_give_to_j = TRUE
               }
               
-              state_j = GenerateStateIndex(i, j, k+1, queue_size)
-              pij[state_i,state_j] = (arrival_rate * sum_prob)
-              
-              for(z in 2:queue_size)
-              {
-                if( (k - z) > 0 )
-                {
-                  state_j = GenerateStateIndex(i, j+z-1, k-z, queue_size)
-                  pij[state_i,state_j] = sum_prob*i*(portion_complex^(z-1))*(1.0-portion_complex)
-                  
-                }
-              }
-              
-              state_j = GenerateStateIndex(i-1, j+k, 0, queue_size)
-              pij[state_i,state_j] = sum_prob*i*(portion_complex^k)
+              give_to_k = TRUE
             }
           }
         }
+        
+        if(take_from_i == TRUE)
+        {
+          state_j = GenerateStateIndex(i-1, j, k, queue_size)
+          pij[state_i,state_j] = i*(service_rate * sum_prob)
+        }
+        
+        if(give_to_i == TRUE)
+        {
+          state_j = GenerateStateIndex(i+1, j, 0, queue_size)
+          pij[state_i,state_j] = (arrival_rate * sum_prob)*(1.0-portion_complex)
+        }
+        
+        if(take_from_j == TRUE)
+        {
+          state_j = GenerateStateIndex(i, j-1, k, queue_size)
+          pij[state_i,state_j] = (m_service_rate * sum_prob)
+        }
+        
+        if(give_to_j == TRUE)
+        {
+          state_j = GenerateStateIndex(i, j+1, k, queue_size)
+          pij[state_i,state_j] = (arrival_rate * sum_prob)*portion_complex
+        }
+        
+        if(take_from_k == TRUE)
+        {
+          state_j = GenerateStateIndex(i, j, k-1, queue_size)
+          pij[state_i,state_j] = ((customer_leave_rate +(i*service_rate*(1.0-portion_complex))) * sum_prob)
+        }
+        
+        if(give_to_k == TRUE)
+        {
+          state_j = GenerateStateIndex(i, j, k+1, queue_size)
+          pij[state_i,state_j] = (arrival_rate * sum_prob)
+        }
+        
+        if(complex_give_to_j == TRUE)
+        {
+          for(z in 2:queue_size)
+          {
+            if( (k - z) >= 0 )
+            {
+              state_j = GenerateStateIndex(i, j+z-1, k-z, queue_size)
+              pij[state_i,state_j] = sum_prob*i*service_rate*(portion_complex^(z-1))*(1.0-portion_complex)
+            }
+          }
+          
+          state_j = GenerateStateIndex(i-1, j+k, 0, queue_size)
+          pij[state_i,state_j] = sum_prob*i*service_rate*(portion_complex^k)
+        }
+        
       }
     }
   }
@@ -148,6 +172,15 @@ ProblemCSimulation <- function(servers, queue_size, portion_complex, arrival_rat
       pij = pij[-invalid_rows[i],]
       pij = pij[,-invalid_rows[i]]
       qi = qi[-invalid_rows[i]]
+    }
+  }
+  
+  for(i in 1:length(pij[,1]))
+  {
+    sum = sum(pij[i,])
+    if(sum < 0.999)
+    {
+      stop_here = TRUE
     }
   }
   
